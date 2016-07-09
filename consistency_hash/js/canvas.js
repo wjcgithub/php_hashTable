6ee0a43ea55f1697c136cda54c24021954b966fe
wjcgithub <wjc163@sina.cn>
2016-07-09 13:37:19 +0800

    del commit

--- consistency_hash/js/canvas.js
+++ consistency_hash/js/canvas.js
@@ -30,29 +30,16 @@
 
   //绘制图片
   Canvas.prototype.drawImg = function(){
-          var _this = this;
-          var img = new Image();
-          img.src = '../img/rhino.jpg';
+      var _this = this;
+      var img = new Image();
+      img.src = '../img/rhino.jpg';
       img.onload = function(){
-                  //_this.ctx.drawImage(img,0,0);
-                  _this.ctx.transform(1,1,0,1,0,0);
-                  _this.ctx.drawImage(img,50,50,100,100);
-                  //_this.ctx.drawImage(img,10,10,100,100,10,10,100,100);
-                  //_this.ctx.drawImage(img,10,10,100,100,150,150,1000,1000);
-              
+          //_this.ctx.drawImage(img,0,0);
+          _this.ctx.transform(1,1,0,1,0,0);
+          _this.ctx.drawImage(img,50,50,100,100);
+          //_this.ctx.drawImage(img,10,10,100,100,10,10,100,100);
+          //_this.ctx.drawImage(img,10,10,100,100,150,150,1000,1000);
       }
-          //参数值
-          //参数值           描述
-          //img                规定要使用的图像、画布或视频。
-          //sx                可选。开始剪切的 x 坐标位置。
-          //sy                可选。开始剪切的 y 坐标位置。
-          //swidth        可选。被剪切图像的宽度。
-          //sheight    可选。被剪切图像的高度。
-          //x                    在画布上放置图像的 x 坐标位置。
-          //y                    在画布上放置图像的 y 坐标位置。
-          //width        可选。要使用的图像的宽度。（伸展或缩小图像）
-          //height        可选。要使用的图像的高度。（伸展或缩小图像）
-
   }
 
 /**
