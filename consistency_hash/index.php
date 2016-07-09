9452123ef8560f3b11a05a1f7ec3aea60addc437
wjcgithub <wjc163@sina.cn>
2016-07-09 17:36:11 +0800

    添加一致性hash查找

--- consistency_hash/index.php
+++ consistency_hash/index.php
@@ -8,7 +8,38 @@
      * license GPL
      */
     include_once "MemServerSetting.php";
+    include_once "FlexiHash.php";
     $mObj = MemServerSetting::getInstance();
-    print_r($mObj->getMemServer('name'));
-    print_r($mObj->getMemServer('age'));
-    print_r($mObj->getMemServer('sex'));
+//    print_r($mObj->getMemServer('name'));
+//    print_r($mObj->getMemServer('age'));
+//    print_r($mObj->getMemServer('sex'));
+
+    $FlexiObj = new FlexiHash();
+    $FlexiObj->addServer('192.168.1.123');
+    $FlexiObj->addServer('192.168.1.124');
+    $FlexiObj->addServer('192.168.1.125');
+    $FlexiObj->addServer('192.168.1.126');
+    $FlexiObj->addServer('192.168.1.127');
+    $FlexiObj->addServer('192.168.1.227');
+    $FlexiObj->addServer('192.168.1.7');
+    $FlexiObj->addServer('192.168.1.197');
+
+    print_r($FlexiObj->getAllServer());
+
+    echo $FlexiObj->lookup('name');
+    echo "\r\n";
+    echo $FlexiObj->lookup('name1');
+    echo "\r\n";
+    echo $FlexiObj->lookup('name2');
+    echo "\r\n";
+    echo $FlexiObj->lookup('age');
+    echo "\r\n";
+
+
+    $FlexiObj->removeServer('192.168.1.126');
+    $FlexiObj->removeServer('192.168.1.127');
+    $FlexiObj->removeServer('192.168.1.227');
+    $FlexiObj->removeServer('192.168.1.7');
+    $FlexiObj->removeServer('192.168.1.197');
+
+    print_r($FlexiObj->getAllServer());
